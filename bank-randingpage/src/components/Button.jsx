3ab8d85da229bb0a4bf1736fa86992f3529b67bd
bishoe01 <bishoe01@gmail.com>
2023-07-31 15:30:00 +0900

    Testimonials , Clients 섹션 추가

--- bank-randingpage/src/components/Button.jsx
+++ bank-randingpage/src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ styles }) {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient ${styles} font-poppins font-medium text-[18px] text-primary outline-none`}
+      className={`py-4 px-6 bg-blue-gradient ${styles} font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px]`}
     >
       Get Started
     </button>
